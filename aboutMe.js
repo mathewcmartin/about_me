121ee90b282790a9b083d4285d9c64dc555c6461
Mathew C Martin <chactasstryker@gmail.com>
2017-03-07 15:16:59 -0800

    Committing files for the first time to the branch.

---
+++ aboutMe.js
@@ -0,0 +1,40 @@
+
+var middleName = prompt('Can you guess my middle name') {
+} if (middleName === 'Clifford') {
+    alert('Wow, you guessed my middle name.');
+    console.log(middleName);
+} else {}
+    alert('Sorry, my middle name is actually Clifford.');
+}
+
+var interests = prompt('Can you guess one of my interests?') {
+} if (interest === 'coding') {
+    alert('Nice, you guessed my primary interest is coding.');
+    console.log(interest);
+} else {
+    alert('Actually my primary interest is coding.')
+}
+
+var interestingFact = prompt('How many public schools do you think I attended thru High School?') {
+} if (interestingFact === '6') {
+    alert('You are good! I did attend 6 schools');
+    console.log(interestingFact);
+} else {
+    alert('Nope, it was 6 public schools. I was a military brat and moved quite a lot growing up.');
+}
+
+var careerGoals = prompt('What do you think would be my ideal job?') {
+} if (careerGoals === 'Cartographer') {
+    alert('You must have a crystal ball! Yep, being a Cartographer is in fact my career goal');
+    console.log(careerGoals);
+} else {
+    alert('Well almost... my career goal is actually to be a Cartographer.')
+}
+
+var focus = prompt('What computer language do you think I\'m focusing my studies?') {
+} if (focus === 'Python') {
+    alert('Impressive, you are on the money!');
+    console.log(focus);
+} else {
+    alert('Actually, I\'m focusing my study on Python, as it is apparently the current standard in the GIS industry');
+}
