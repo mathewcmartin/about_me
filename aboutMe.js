b55c9d9138db3fda4a615bb1971977d37420b706
Mathew C Martin <chactasstryker@gmail.com>
2017-03-07 15:54:51 -0800

    third commit of day 2 homework files

--- aboutMe.js
+++ aboutMe.js
@@ -1,38 +1,43 @@
-
-var middleName = prompt('Can you guess my middle name') {
-} if (middleName === 'Clifford') {
-    alert('Wow, you guessed that my middle name is Cliffor.');
+'use strict';
+var middleName = prompt('Can you guess if my middle name is Charles? \(Yes or No\)') {
+  middleName.toLowerCase();
+} if (middleName === 'no') {
+    alert('Wow, you guessed that my middle name is not Charles. It\'s actually Clifford.');
     console.log(middleName);
-} else {}
+} else {
     alert('Sorry, my middle name is actually Clifford.');
 }
 
-var interests = prompt('Can you guess one of my interests?') {
-} if (interest === 'coding') {
+var interests = prompt('Can you guess one if I\'m interested in coding?') {
+  interests.toLowerCase():
+} if (interest === 'yes') {
     alert('Nice, you guessed my primary interest is coding.');
     console.log(interest);
 } else {
-    alert('Actually my primary interest is coding.')
+    alert('Actually, my primary interest is coding.')
 }
 
-var interestingFact = prompt('How many public schools do you think I attended thru High School?') {
-} if (interestingFact === '6') {
-    alert('You are good! I did attend 6 schools');
+var interestingFact = prompt('Do you think I attended more than 5 public schools thru High School?') {
+  interestingFact.toLowerCase();
+} if (interestingFact === 'yes') {
+    alert('You are good! I did attend more than 5 schools. I actually attended 6 schools as a \'military brat\' growing up.');
     console.log(interestingFact);
 } else {
     alert('Nope, it was 6 public schools. I was a military brat and moved quite a lot growing up.');
 }
 
-var careerGoals = prompt('What do you think would be my ideal job?') {
-} if (careerGoals === 'Cartographer') {
+var careerGoals = prompt('Do you think my ideal job is that as a Cartographer?') {
+  careerGoals.toLowerCase();
+} if (careerGoals === 'yes') {
     alert('You must have a crystal ball! Yep, being a Cartographer is in fact my career goal');
     console.log(careerGoals);
 } else {
-    alert('Well almost... my career goal is actually to be a Cartographer.')
+    alert('Well almost... my career goal is in fact to be a Cartographer.')
 }
 
-var focus = prompt('What computer language do you think I\'m focusing my studies?') {
-} if (focus === 'Python') {
+var focus = prompt('Is Python the computer language you think I\'m focusing my studies?') {
+  focus.toLowerCase();
+} if (focus === 'yes') {
     alert('Impressive, you are on the money!');
     console.log(focus);
 } else {
